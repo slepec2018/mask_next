3a3344136de0f57c3d7557dcd8c43bab589e1c46
slepec <slepec2018@gmail.com>
2024-01-05 14:34:07 +0000

    add_users_to_widget

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -2,8 +2,9 @@ import { SearchIcon } from '@heroicons/react/outline';
 import React, { useState } from 'react';
 import News from './News';
 
-export default function Widgets({ newsResults }) {
+export default function Widgets({ newsResults, randomUsersResults }) {
   const [articleNum, setArticleNum] = useState(3);
+  const [randomUserNumber, setRandomUserNumber] = useState(3);
 
   return (
     <div
@@ -43,6 +44,53 @@ export default function Widgets({ newsResults }) {
         <button
           className='text-blue-300 pl-4 pb-3 hover:text-blue-400'
           onClick={() => setArticleNum(articleNum + 3)}
+        >
+          Show more
+        </button>
+      </div>
+      <div
+        className='text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[85%] sticky top-16'
+      >
+        <h4
+          className='font-bold text-xl px-4'
+        >
+          Who to follow
+        </h4>
+        {randomUsersResults.slice(0, randomUserNumber).map((randomUser) => (
+          <div
+            key={randomUser.login.username}
+            className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200'
+          >
+            <img
+              src={randomUser.picture.thumbnail}
+              alt="random user img"
+              width="40"
+              className='rounded-full'
+            />
+            <div
+              className='truncate ml-4 leading-5'
+            >
+              <h4
+                className='text-bold hover:underline text-[14px] truncate'
+              >
+                {randomUser.login.username}
+              </h4>
+              <h5
+                className='text=[13px] text-gray-500 truncate'
+              >
+                {randomUser.name.first + ' ' + randomUser.name.last}
+              </h5>
+            </div>
+            <button
+              className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'
+            >
+              Follow
+            </button>
+          </div>
+        ))}
+        <button
+          onClick={() => setRandomUserNumber(randomUserNumber + 3)}
+          className='text-blue-300 pl-4 pb-3 hover:text-blue-400'
         >
           Show more
         </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
 import Widgets from "@/components/Widgets";
 
-export default function Home({ newsResults }) {
+export default function Home({ newsResults, randomUsersResults }) {
   return (
     <div>
       <main
@@ -12,6 +12,7 @@ export default function Home({ newsResults }) {
         <Feed />
         <Widgets
           newsResults={newsResults.articles}
+          randomUsersResults={randomUsersResults.results}
         />
       </main>
     </div>
@@ -21,10 +22,14 @@ export default function Home({ newsResults }) {
 export async function getServerSideProps() {
   const newsResults = await fetch('https://saurav.tech/NewsAPI/top-headlines/category/business/us.json')
     .then((res) => res.json());
+  
+  const randomUsersResults = await fetch('https://randomuser.me/api/?results=30&inc=name,login,picture')
+    .then((res) => res.json());
 
   return {
     props: {
       newsResults,
+      randomUsersResults
     }
   }
 }
