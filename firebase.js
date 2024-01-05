e847e62d42eef3dd13a9968319799c3408631cc9
slepec <slepec2018@gmail.com>
2024-01-05 15:00:06 +0000

    add_firebase_nextauth

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "mask-next.firebaseapp.com",
+  projectId: "mask-next",
+  storageBucket: "mask-next.appspot.com",
+  messagingSenderId: "840230453648",
+  appId: "1:840230453648:web:8cb811a3d1812be5e03315"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
